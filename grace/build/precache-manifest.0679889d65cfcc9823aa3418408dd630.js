6e85a804f3c7b84f3f262a8f70bfa9616f9d4585
Albie Baker-Smith <bakersmitha@gmail.com>
2019-03-02 16:18:40 +0000

    build

---
+++ grace/build/precache-manifest.0679889d65cfcc9823aa3418408dd630.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c48c03d6d0f59a4f1f26",
+    "url": "/static/css/main.e275f4cc.chunk.css"
+  },
+  {
+    "revision": "c48c03d6d0f59a4f1f26",
+    "url": "/static/js/main.c48c03d6.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "d6cbbdcb9ddb5bc94f2a",
+    "url": "/static/js/2.d6cbbdcb.chunk.js"
+  },
+  {
+    "revision": "94b8a54e11ec53296622c0af4c63febb",
+    "url": "/index.html"
+  }
+];
--- grace/src/API.js
+++ grace/src/API.js
@@ -1,5 +1,5 @@
 export function apiUrl(s) {
-  return 'http://10.0.100.86:5000' + s;
+  return 'localhost:5000' + s;
 }
 
 export function postJSON(s, obj) {
